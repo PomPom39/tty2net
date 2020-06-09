f907a6d145051bc0c61def290e1b57fe686bdd34
Mahesh Devadiga <65815541+PomPom39@users.noreply.github.com>
2020-06-09 19:13:49 +0530

    updated header file for s2e
    Note: dont know if this will work with cmake build, but its tested as a eclipse project

--- include/s2e_ess.h
+++ include/s2e_ess.h
@@ -1,50 +1,50 @@
-#ifndef _S2E_ESS_H_
-#define _S2E_ESS_H_
+/*
+ * s2e_ess.h
+ *
+ *  Created on: 09-Jun-2020
+ *      Author: root
+ */
 
+#ifndef S2E_ESS_H_
+#define S2E_ESS_H_
 
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <string.h>
-#include <arpa/inet.h>
-
-
-
+#include <stdio.h>
+#include <fcntl.h>
 #define NET_MODE_SERVER 1
 #define NET_MODE_CLIENT 2
-
-
 #define NET_PROTO_UDP 0
 #define NET_PROTO_TCP 1
 
 
-
-struct s2e_t {
-
+struct s2e_conf {
 	char tty_device[16];
+	long tty_baudrate;
+	int tty_parity;
+	int tty_stopbit;
 	int net_mode;
 	int net_proto;
 	int net_port;
-
 	int tty_fd;
 	int sock_fd;
-
-
-	char *tty_buffer;
+	char tty_buffer[256];
 	int tty_buffsz;
-	char *net_buffer;
+	char net_buffer[256];
 	int net_buffsz;
-
-	int tty_rcvd;
-	int tty_sent;
-	int net_rcvd;
-	int net_sent;
 
 };
 
 
 
+int net_open(struct s2e_conf *conf);
+int net_close(struct s2e_conf *conf);
+int net_read(struct s2e_conf *conf, char *buffer, int size);
+int net_write(struct s2e_conf *conf, char *buffer, int size);
+int tty_open(struct s2e_conf *conf);
+int tty_close(struct s2e_conf *conf);
+int tty_write(struct s2e_conf *conf, char *buffer, int size);
+int tty_read(struct s2e_conf *conf, char *buffer, int size);
 
-int net_open(struct s2e_t *s2e);
-int test();
-
-
-#endif
+#endif /* S2E_ESS_H_ */
